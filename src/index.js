3d3fcbc4e182f3f8861b0743d9c4311437576fd7
Levi Winans <levieiko@gmail.com>
2021-06-22 11:31:16 -0700

    import firebase on index

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import firebase from "firebase/app";
+import 'firebase/firestore';
+
+import 'firebase/firebase-storage'
+
+  firebase.initializeApp({
+    apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+    authDomain: "prana-wellness.firebaseapp.com",
+    databaseURL: "https://prana-wellness.firebaseio.com",
+    projectId: "prana-wellness",
+    storageBucket: "prana-wellness.appspot.com",
+    messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+    appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+    measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
+  });
+  const storage = firebase.storage();
+  export { storage, firebase as default }
+
+
+  ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, 
+    document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
