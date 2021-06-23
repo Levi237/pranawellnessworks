42a215f09cfea29137347c0566099f2214737db3
Levi Winans <levieiko@gmail.com>
2021-06-23 11:00:02 -0700

    break down state info, add style

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -4,29 +4,49 @@ import styled from 'styled-components';
 export default class EventsList extends Component {
     state = {
         eventsList: [{
-            title: "Virtual Prenatal Yoga",
-            info: "Every Monday 5p PT- Virtual, donation based Prenatal Yoga",
+            title: "Prenatal Yoga",
+            location: "virtual",
+            info: "add more description here",
+            price: "donation based",
+            when: "Every Monday 5p PT",
             note: "(happening now)",
+            img: "./events/prenatal.png",
             link: "https://mailchi.mp/8c8ba20bd281/prenatal-mondays"
           },{
-            title: "Virtual Postpartum Yoga",
-            info: "Postpartum Yoga- every 1st & 3rd Tuesday virtual, donation based 5p PT",
+            title: "Postpartum Yoga",
+            location: "virtual",
+            info: "add more description here",
+            price: "donation based",
+            when: "every 1st & 3rd Tuesday @5p PT",
             note: "(starting July 6th)",
+            img: "./events/postpartum.png",
             link: "https://mailchi.mp/fcd82697d0c7/postpartumtuesdays"
           },{
-            title: "Virtual Yoga Flow",
-            info: "Thursdays Virtual Yoga Flow 5p PT- donation based",
+            title: "Yoga Flow",
+            location: "virtual",
+            info: "add more description here",
+            price: "donation based",
+            when: "Thursdays @ 5p PT",
             note: "(starting July 8th)",
+            img: "./events/flow.png",
             link: "https://mailchi.mp/f44d1c9f43d6/yogflowthursdays"
           },{
             title: "Meditation + Breathwork",
-            info: "Fridays- Meditation + Breathwork with @thenalaverse -donation based",
+            location: "virtual",
+            info: "with @thenalaverse",
+            price: "donation based",
+            when: "Fridays",
             note: "(happening now)",
+            img: "./events/meditation.png",
             link: "https://thenalaverse.com/get-lifted"
           },{
             title: "Yoga on the Harbor",
-            info: "Yoga on the Harbor, every first Wednesday in San Diego",
+            location: "in San Diego",
+            info: "add more description here",
+            price: "donation based",
+            when: "every first Wednesday",
             note: "(starting July 7th)",
+            img: "./events/harbor.png",
             link: "https://yogaontheharbor.eventbrite.com"
           }]
     };
@@ -36,15 +56,23 @@ export default class EventsList extends Component {
         const showList = eventsList.map((event, k) => {
           return(
             <EventWrapper>
-              <section>x</section>
+              <section><img src={event.img} alt={event.title}/></section>
               <section key={k}>
-              <a href={event.link} alt={event.title}>{event.title}</a>
-              <p>{event.info}</p>
-              <p>{event.note}</p>
-              <a href={event.link} alt={event.title}>
-                  Sign Up&nbsp;
-                  <span>→</span>
-              </a>
+                <div>
+                  <a href={event.link} alt={event.title}>
+                    <h1>{event.title}</h1>
+                  </a>
+                  <h4>[ {event.location} ]</h4>
+                  <p>
+                    {event.info}
+                    <small>{event.note}</small>
+                  </p>
+                  <a href={event.link} alt={event.title} target="_blank">
+                    <span></span>
+                    &nbsp;Sign Up&nbsp;
+                    <span></span>
+                  </a>
+                </div>
               </section>
             </EventWrapper>
           );
@@ -56,38 +84,85 @@ export default class EventsList extends Component {
 };
 
 const EventWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
 
-`;
-
-const ListWrapper = styled.div`
-  width: 100%;
-  > div {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-template-areas: 'left right';
-    &:nth-of-type(even) {
-      > section {
-        &:first-of-type {
-          grid-area: left;
-          background-color: pink;
-        }
-        &:last-of-type {
-          grid-area: right;
-          background-color: blue;
-        }
-      }
+  a {
+    text-decoration: none;
+    &:hover {
+      border-bottom: 1px solid grey;
     }
-    &:nth-of-type(odd) {
-      > section {
-        &:first-of-type {
-          grid-area: right;
-          background-color: green;
-        }
-        &:last-of-type {
-          grid-area: left;
-          background-color: yellow;
-        }
+    &:last-of-type {
+      font-size: 3vw;
+      color: var(--purple);
+      &:hover {
+        color: var(--green);
       }
     }
   }
+
+  h1 {
+    font-size: 4.2vw;
+    letter-spacing: .2vw;
+    padding: 0;
+  }
+  h4 {
+    font-size: 2.33vw;
+    font-weight: 100;
+    padding: 0;
+    margin: 1vw 0 0;
+  }
+  p {
+   font-size: 2.8vw;
+   padding: 0;
+   margin: 2vw 0; 
+   color: var(--grey);
+   > small {
+     display: block;
+     font-weight: 100;
+   }
+  }
+
+  &:nth-of-type(even) {
+    grid-template-areas: 'img text';
+    a > span:first-of-type {
+      &::after {
+        content: "<";
+      }
+    }
+  }
+  &:nth-of-type(odd) {
+    grid-template-areas: 'text img';
+    a > span:last-of-type {
+      &::after {
+        content: ">";
+      }
+    }
+  }
+
+  > section {
+    width: 50vw;
+    height: 50vw;
+
+    &:first-of-type {
+      grid-area: img;
+      > img {
+        width: 100%;
+      }
+    }
+    &:last-of-type {
+      grid-area: text;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      > div {
+        padding: 0 10%;
+      }
+    }
+  }
+`;
+
+const ListWrapper = styled.div`
+  width: 100vw;
 `;
