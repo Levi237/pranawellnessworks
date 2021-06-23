5c85f3375600bcd4e33ce0e3931505f42277538e
Levi Winans <levieiko@gmail.com>
2021-06-23 15:43:00 -0700

    add description and media query for mobile

--- src/components/EventsList.js
+++ src/components/EventsList.js
@@ -6,27 +6,27 @@ export default class EventsList extends Component {
         eventsList: [{
             title: "Prenatal Yoga",
             location: "virtual",
-            info: "add more description here",
+            info: "Guided movement for support, empowerment, and education.",
             price: "donation based",
-            when: "Every Monday 5p PT",
+            when: "Every Monday @ 5p PT",
             note: "(happening now)",
             img: "./events/prenatal.png",
             link: "https://mailchi.mp/8c8ba20bd281/prenatal-mondays"
           },{
             title: "Postpartum Yoga",
             location: "virtual",
-            info: "add more description here",
+            info: "Build confidence, strengthen, and renew the body. ",
             price: "donation based",
-            when: "every 1st & 3rd Tuesday @5p PT",
+            when: "Every 1st & 3rd Tuesday @ 5p PT",
             note: "(starting July 6th)",
             img: "./events/postpartum.png",
             link: "https://mailchi.mp/fcd82697d0c7/postpartumtuesdays"
           },{
             title: "Yoga Flow",
             location: "virtual",
-            info: "add more description here",
+            info: "Join us from anywhere! All levels yoga flow.",
             price: "donation based",
-            when: "Thursdays @ 5p PT",
+            when: "Every Thursday @ 5p PT",
             note: "(starting July 8th)",
             img: "./events/flow.png",
             link: "https://mailchi.mp/f44d1c9f43d6/yogflowthursdays"
@@ -35,16 +35,16 @@ export default class EventsList extends Component {
             location: "virtual",
             info: "with @thenalaverse",
             price: "donation based",
-            when: "Fridays",
+            when: "Every Friday @ 10p PT",
             note: "(happening now)",
             img: "./events/meditation.png",
             link: "https://thenalaverse.com/get-lifted"
           },{
             title: "Yoga on the Harbor",
             location: "in San Diego",
-            info: "add more description here",
+            info: "Join us in person, for a fun community flow!",
             price: "donation based",
-            when: "every first Wednesday",
+            when: "Every first Wednesday @ 6p PT",
             note: "(starting July 7th)",
             img: "./events/harbor.png",
             link: "https://yogaontheharbor.eventbrite.com"
@@ -71,7 +71,7 @@ export default class EventsList extends Component {
                   </p>
                   <a href={event.link} alt={event.title} target="_blank">
                     <span></span>
-                    &nbsp;Sign Up&nbsp;
+                    &emsp;Sign Up&emsp;
                     <span></span>
                   </a>
                 </div>
@@ -86,13 +86,10 @@ export default class EventsList extends Component {
 };
 
 const EventWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-
   a {
     text-decoration: none;
     &:hover {
-      border-bottom: 1px solid grey;
+      border-bottom: 1px solid var(--green);
     }
     &:last-of-type {
       font-size: 3vw;
@@ -111,40 +108,34 @@ const EventWrapper = styled.div`
   h4 {
     font-size: 2.33vw;
     font-weight: 100;
+    color: var(--grey);
     padding: 0;
     margin: 1vw 0 0;
   }
 
   p {
-   font-size: 2.8vw;
-   padding: 0;
-   margin: 2vw 0; 
-   color: var(--grey);
-   > small {
-     display: block;
-     font-weight: 100;
-   }
+    font-size: 2.8vw;
+    padding: 0;
+    margin: 2vw 0; 
+    color: var(--grey);
+    > small {
+      display: block;
+      font-weight: 100;
+      marign-top: 1vw;
+    }
   }
 
-  &:nth-of-type(even) {
-    grid-template-areas: 'img text';
-    a > span:first-of-type {
-      &::after {
-        content: "<";
-      }
-    }
+  a > span::after {
+    -webkit-transform:scale(1.0, 2.5);
+    -moz-transform:scale(1.0, 2.5);
+    -ms-transform:scale(1.0, 2.5);
+    -o-transform:scale(1.0, 2.5);
+    transform:scale(1.0, 2.5);
+    font-weight: 100;
+    display: inline-block;
   }
-  &:nth-of-type(odd) {
-    grid-template-areas: 'text img';
-    a > span:last-of-type {
-      &::after {
-        content: ">";
-      }
-    }
-  }
+  
   > section {
-    width: 50vw;
-    height: 50vw;
     &:first-of-type {
       grid-area: img;
       background-size: 100%;
@@ -171,8 +162,64 @@ const EventWrapper = styled.div`
       }
     }
   }
+  @media screen and (max-width: 640px){
+    h1 {
+      font-size: 7.5vw;
+    }
+    h4 {
+      font-size: 4vw;
+    }
+    p {
+      font-size: 6vw;
+      margin: 5vw 0;
+    }
+    a {
+      &:last-of-type {
+        font-size: 6vw;
+      }
+    }
+  }
 `;
 
 const ListWrapper = styled.div`
   width: 100vw;
+  > div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+
+    &:nth-of-type(even) {
+      grid-template-areas: 'img text';
+      a > span:first-of-type {
+        &::after {
+          content: "<";
+        }
+      }
+    }
+    &:nth-of-type(odd) {
+      grid-template-areas: 'text img';
+      a > span:last-of-type {
+        &::after {
+          content: ">";
+        }
+      }
+    }
+
+    > section {
+      width: 50vw;
+      height: 50vw;
+    }
+  }
+  @media screen and (max-width: 640px){
+    > div {
+      grid-template-columns: 1fr;
+      grid-template-rows: 1fr 1fr;
+      grid-template-areas: 
+        'img' 
+        'text'!important;
+      > section {
+        width: 100vw;
+        height: 100vw;
+      }
+    }
+  }
 `;
