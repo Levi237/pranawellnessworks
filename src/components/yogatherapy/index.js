04821405bdce3af0d9d72334d345f8f1be766833
Levi Winans <levieiko@gmail.com>
2021-06-29 12:20:02 -0700

    remove banner redundancy

--- src/components/yogatherapy/index.js
+++ src/components/yogatherapy/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
-import WhyYogaTherapy  from './WhyYogaTherapy';
 import TwoColumnFeature  from './TwoColumnFeature';
+import FloralBox          from '../banners/FloralBoxComponent';
 
-const YogaTherapyMain = () => {
-    return(
-      <>
-        <WhyYogaTherapy />
-        <TwoColumnFeature />
-        <VideoBox src="https://player.vimeo.com/video/488789376"></VideoBox>
-      </>);
+export default class YogaTherapyMain extends Component {
+  state = {
+    info: { 
+        title: "Why Yoga Therapy?",
+        subtitle: "WHAT I SAY TO THOSE WHO HAVEN'T TRIED IT",
+        content: "Most people are unaware of the power they possess within their own body to heal itself. Yoga Therapy is a personal, tailored approach to healing both mental and physical challenges. It is the perfect marriage of Eastern and Western medicine, and an alternative to traditional methods. Every session with me, you will feel safe and free to exist in your own body and in your personal truth. Yoga Therapy is specific and focused, using traditional practices to address health challenges, reduce symptoms, restore balance, manage a condition, increase vitality, and improve mental health.",
+        image: "yoga-therapy.jpg",
+        servicesTitle: "HOW CAN WE WORK TOGETHER?",
+        skills: ["none"],
+        services: [
+            'Private one on one Yoga Therapy',
+            'Onsite Yoga Therapist at your medical office',
+            'Focused workshops for your clients:',
+            'lower back, shoulder opener; reduce anxiety'
+        ],
+        boxOne: "right",
+        boxTwo: "left",
+        backgroundColor: "#E0CEE5",
+        subtitleColor: "white",
+        route: false,
+        cta: "",
+        ctaColor: "",
+    }
+};
+render(){
+  return(
+    <>
+      <FloralBox passedInfo={this.state.info} />
+      <TwoColumnFeature />
+      <VideoBox src="https://player.vimeo.com/video/488789376"></VideoBox>
+    </>);
+}
 };
 
 const VideoBox = styled.embed`
@@ -25,5 +50,3 @@ const VideoBox = styled.embed`
       margin: 5vw auto 0;
     }
 `;
-
-export default YogaTherapyMain;
