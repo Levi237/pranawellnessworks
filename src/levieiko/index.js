ec90654f2971d70c60ae80be0b74785b0e798841
Levi Winans <levieiko@gmail.com>
2021-06-30 01:42:52 -0700

    camelcase

--- src/levieiko/index.js
+++ src/levieiko/index.js
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const LeviEikoBrand = () => {
     const svgLogo = 
     <svg version="1.1" width="100%" height="100%" viewBox="0 0 500 500">
-      <rect fill="transparent" stroke-width="0" width="500" height="500" x="0" y="0" />
-      <path stroke-width="0" 
+      <rect fill="transparent" strokeWidth="0" width="500" height="500" x="0" y="0" />
+      <path strokeWidth="0" 
         d="M0 0 H75 V300 H265 V425 H500 V500 H0, M140 0 H500 V125 H265 V250 H140, M325 175 H500 V375 H325 V300 H425 V250 H325 Z"/>
     </svg>
 
