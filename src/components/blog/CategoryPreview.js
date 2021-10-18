0b7a1ebbe16e56536b32c1a872cbbac3d9121f27
Levi237 <levieiko@gmail.com>
2021-10-18 00:39:01 -0700

    add mobile style

--- src/components/blog/CategoryPreview.js
+++ src/components/blog/CategoryPreview.js
@@ -87,6 +87,30 @@ const FeatureBox = styled.section`
             text-align: center!important;
         }
     }
+    @media screen and (max-width: 640px) {
+        a {
+            h1 { 
+                margin-top: 4vw;
+                font-size: 5vw;
+                line-height: 5vw;
+            }
+            h4 {
+                margin-top: 1vw;
+                font-size: 3vw;
+                line-height: 3vw;
+                color: var(--grey);
+            }
+    
+            > section:first-of-type {
+                height: 50vw;
+                width: 100%;
+                text-align: center!important;
+            }
+            > section:last-of-type {
+                height: auto;
+            }
+        }
+    }
 `;
 const ThreeRows = styled.section`
     width: 100%;
@@ -121,6 +145,36 @@ const ThreeRows = styled.section`
             &:last-of-type {
                 height: 8vw;
                 width: calc(26vw - 1px);
+            }
+        }
+    }
+    @media screen and (max-width: 640px) {
+        a {
+            width: 100%;
+
+            h1 { 
+                font-size: 4.5vw;
+                text-align: left;
+                line-height: 120%;
+                padding: 0;
+                margin-top: 1px;
+            }
+            p {
+                margin-top: 1vw;
+                font-size: 3.2vw;
+                line-height: 120%;
+                color: var(--grey);
+            }
+
+            > section {
+                &:first-of-type {
+                    height: 24vw;
+                    width: 38vw;
+                }
+                &:last-of-type {
+                    height: 24vw;
+                    width: calc(60vw - 1px);
+                }
             }
         }
     }
@@ -173,6 +227,9 @@ a {
         }
 }
     @media screen and (max-width: 640px) {
+        > h2 {
+            font-size: 4vw;
+        }
         // a {
         //     h1 { 
         //         font-size: 4.25vw;
@@ -200,28 +257,29 @@ a {
         > div {
             width: 100vw;
             margin: 0 auto 5vw;
-            // > a {
-            //     > section {
-            //         text-align: left;
-            //         width: 50vw;
-            //         height: 65vw;
-            //         display: inline-block;
-            //         vertical-align: top; 
 
-            //         &:first-of-type {
-            //             margin-right: 0;
-            //             > * {
-            //                 padding: 0 5vw;
-            //             }
-            //         }
-            //         &:last-of-type {
-            //             background-position: center;
-            //             background-size: cover;
-            //             background-repeat: no-repeat;
-            //             float: left;
-            //         }
-            //     }
-            }
+                > section {
+                    text-align: left;
+                    width: 100vw;
+                    height: auto;
+                    display: inline-block;
+                    vertical-align: top; 
+
+                    &:first-of-type {
+                        margin-right: 0;
+                        margin-bottom: 5vw;
+                        > * {
+                            padding: 0 5vw;
+                        }
+                    }
+                    &:last-of-type {
+                        background-position: center;
+                        background-size: cover;
+                        background-repeat: no-repeat;
+                        float: left;
+                    }
+                }
+
         }
     }
 `;
