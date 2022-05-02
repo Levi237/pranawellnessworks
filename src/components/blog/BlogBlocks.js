ff291fb4738ddebc7be72635f717ce4718c284cf
Levi237 <levieiko@gmail.com>
2022-05-01 17:59:40 -0700

    clean up

--- src/components/blog/BlogBlocks.js
+++ src/components/blog/BlogBlocks.js
@@ -58,51 +58,50 @@ const BlockColumns = styled.div`
     text-align: left;
     width: 88vw;
     margin: 0 auto;
+    
+    > div {
+      width: 22vw;
+      display: inline-block;
+      vertical-align: top;
+      text-align: left;
+
+      &:first-of-type {
+        display: none;
+      }
+      &:nth-of-type(2) {
+        display: none;
+      }
+      &:nth-of-type(3) {
+        display: none;
+      }
+      .img {
+        height: 11vw;
+        margin: 0 auto;
+        width: 20vw;
+        background-size: cover;
+        background-position: center;
+      }
+      .txt {
+        height: 14vw;
+      }
+    }
   }
 
-  > div > div {
-    width: 22vw;
-    display: inline-block;
-    vertical-align: top;
-    text-align: left;
-
-    &:first-of-type {
-      display: none;
+  @media screen and (max-width: 945px) {
+    h1 {
+      font-size: 2.5vw;  
     }
-    &:nth-of-type(2) {
-      display: none;
+    h4{
+      font-size: 1.75vw;
     }
-    &:nth-of-type(3) {
-      display: none;
-    }
-    .img {
-      height: 11vw;
-      margin: 0 auto;
-      width: 20vw;
-      background-size: cover;
-      background-position: center;
-    }
-    .txt {
-      height: 14vw;
-    }
-  }
-
-    @media screen and (max-width: 945px) {
-      h1 {
-        font-size: 2.5vw;  
+    p {
+      small {
+        font-size: 1.5vw;
       }
-      h4{
-        font-size: 1.75vw;
-      }
-      p {
-        small {
-          font-size: 1.5vw;
-        }
-      }
+    }
+    > div {
+      width: 100vw!important;
       > div {
-        width: 100vw!important;
-      }
-      > div > div {
         width: 45vw;
         .img {
           height: 22vw;
@@ -112,42 +111,43 @@ const BlockColumns = styled.div`
           height: 30vw;
           width: 41vw;
           margin: 0 auto;
-           p {
-             padding: 5px 0 0;
-           }
-        }
-      }
-    }
-    @media screen and (max-width: 640px) {
-        h1 {
-          font-size: 18px;  
-          padding: 0 0 20px;
-        }
-        h4{
-          font-size: 4vw;
-          padding: 0;
-        }
-        p {
-          padding: 10px 0;
-          small {
-            font-size: 10px;
-          }
-        }
-        > div {
-          width: 42.5vw;
-          &:nth-of-type(odd) {
-            margin-right: 5vw;
-          }
-          .img {
-            height: 45vw;
-            width: 100%;
-          }
-          .txt {
-            height: auto;
+          p {
+            padding: 5px 0 0;
           }
         }
       }
     }
+  }
+  @media screen and (max-width: 640px) {
+      h1 {
+        font-size: 18px;  
+        padding: 0 0 20px;
+      }
+      h4{
+        font-size: 4vw;
+        padding: 0;
+      }
+      p {
+        padding: 10px 0;
+        small {
+          font-size: 10px;
+        }
+      }
+      > div {
+        width: 42.5vw;
+        &:nth-of-type(odd) {
+          margin-right: 5vw;
+        }
+        .img {
+          height: 45vw;
+          width: 100%;
+        }
+        .txt {
+          height: auto;
+        }
+      }
+    }
+  }
 `;
 
 export default BlogBlocks;
